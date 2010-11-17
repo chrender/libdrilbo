dfda66dfa1772dc40de5a7a3a146d5560b793fc4
chrender <chrender>
2010-11-17 19:34:06 +0000

    - Added reference to original code from Mark Howell. Thanks!

--- src/drilbo/drilbo-mg1.c
+++ src/drilbo/drilbo-mg1.c
@@ -27,6 +27,12 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * 
+ * This module is an adapted version of Mark Howell's pix2gif utility from
+ * the ztools package version 7.3.1, available from:
+ * http://www.ifarchive.org/if-archive/infocom/tools/ztools/ztools731.tar.gz
+ * 
  */
 
 
