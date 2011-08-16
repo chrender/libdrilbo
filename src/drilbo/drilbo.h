476f218680b4c699fe6fa3c0c1e02875364f0a38
chrender <chrender>
2011-08-16 08:44:42 +0000

    - Updared copyright notices.

--- src/drilbo/drilbo.h
+++ src/drilbo/drilbo.h
@@ -3,7 +3,7 @@
  *
  * This file is part of fizmo.
  *
- * Copyright (c) 2009-2010 Christoph Ender.
+ * Copyright (c) 2010-2011 Christoph Ender.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -33,7 +33,7 @@
 #ifndef drilbo_h_INCLUDED 
 #define drilbo_h_INCLUDED
 
-#define DRILBO_VERSION "0.2.0-b4"
+#define DRILBO_VERSION "0.2.0-b5"
 
 #ifdef UNUSED 
 #elif defined(__GNUC__) 
